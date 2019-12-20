611d74fb9daef782a4088dc06baa50d749523fd3
jitao <flmnix@163.com>
2019-12-20 18:55:41 +0800

    切到OC

--- ios/Classes/UmengAnalyticsPlugin.m
+++ ios/Classes/UmengAnalyticsPlugin.m
@@ -1,15 +1,20 @@
 #import "UmengAnalyticsPlugin.h"
-#if __has_include(<umeng_analytics_plugin/umeng_analytics_plugin-Swift.h>)
-#import <umeng_analytics_plugin/umeng_analytics_plugin-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "umeng_analytics_plugin-Swift.h"
-#endif
 
 @implementation UmengAnalyticsPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftUmengAnalyticsPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"umeng_analytics_plugin"
+            binaryMessenger:[registrar messenger]];
+  UmengAnalyticsPlugin* instance = [[UmengAnalyticsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
--- ios/umeng_analytics_plugin.podspec
+++ ios/umeng_analytics_plugin.podspec
@@ -14,12 +14,12 @@ A new Flutter plugin.
   s.author           = { 'Your Company' => 'email@example.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
+  s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
-  s.dependency 'UMCCommon'
-  s.dependency 'UMCAnalytics'
+  s.dependency 'UMengAnalytics-NO-IDFA'
+  s.static_framework = true
   s.platform = :ios, '8.0'
 
   # Flutter.framework does not contain a i386 slice. Only x86_64 simulators are supported.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'VALID_ARCHS[sdk=iphonesimulator*]' => 'x86_64' }
-  s.swift_version = '5.0'
 end
